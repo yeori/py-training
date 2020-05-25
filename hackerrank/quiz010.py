4db8a301ee0a071374eca2a22810877497495e81
yeori <yeori.seo@gmail.com>
2020-05-26 04:47:44 +0900

    문서 추가

--- hackerrank/quiz010.py
+++ hackerrank/quiz010.py
@@ -1,3 +1,26 @@
+"""
+# Word Order
+* https://www.hackerrank.com/challenges/word-order/problem
+
+# Dict 구현체
+기본 dictionary 구현체와 입력 순서를 기억하는 OrderedDict 구현체
+
+## OrderedDict
+* https://docs.python.org/3.6/library/collections.html#collections.OrderedDict
+
+## dict()
+* https://docs.python.org/3.6/library/stdtypes.html#dict
+
+```python
+>>> a = dict(one=1, two=2, three=3)
+>>> b = {'one': 1, 'two': 2, 'three': 3}
+>>> c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+>>> d = dict([('two', 2), ('one', 1), ('three', 3)])
+>>> e = dict({'three': 3, 'one': 1, 'two': 2})
+>>> a == b == c == d == e
+True
+```
+"""
 from collections import OrderedDict
 
 def acc(dic, key, v):
